7d5e3abad1cdc60b1e948ed44fb050e0088813df
Pablo Gimeno <pablo.gimeno@urv.cat>
2019-04-27 10:51:11 +0200

    Updated AF.l and llist.h

--- llist.h
+++ llist.h
@@ -16,15 +16,8 @@ llist *llist_create(void *data);
 /* llist_free: Free a linked list */
 void llist_free(llist *list);
 
-/* llist_add_inorder: Add to sorted linked list */
-int llist_add_inorder(void *data, llist *list,
-                       int (*comp)(void *, void *));
-
 /* llist_push: Add to head of list */
 void llist_push(llist *list, void *data);
-
-/* llist_pop: remove and return head of linked list */
-void *llist_pop(llist *list);
 
 /* llist_print: print linked list */
 void llist_print(llist *list);
